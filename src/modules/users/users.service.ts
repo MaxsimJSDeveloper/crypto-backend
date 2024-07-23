1ccf04da5331ad115ceddca5b704710386ae2a41
Holovko Maksym <golovkomaksim852@gmail.com>
2024-07-23 17:29:58 +0300

    createUser

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@nestjs/common';
-import { users } from 'src/moks';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+import * as bcrypt from 'bcrypt';
+
+import { User } from './models/user.model';
+import { CreateUserDTO } from './dto';
+import { AppError } from 'src/common/errors';
 
 @Injectable()
 export class UserService {
-  getUsers() {
-    return users;
+  constructor(
+    @InjectModel(User) private readonly userRepository: typeof User,
+  ) {}
+
+  async hashPassword(password) {
+    return bcrypt.hash(password, 10);
+  }
+
+  async findUserByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
+    const existUser = await this.findUserByEmail(dto.email);
+
+    if (existUser) throw new BadRequestException(AppError.USER_EXIST);
+
+    dto.password = await this.hashPassword(dto.password);
+
+    await this.userRepository.create({
+      firstName: dto.firstName,
+      username: dto.username,
+      email: dto.email,
+      password: dto.password,
+    });
+
+    return dto;
   }
 }
